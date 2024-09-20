420ffbd3989fbcf8e34d854f70111c7869a4d708
osparking <jbpark03@gmail.com>
2024-09-20 14:23:45 +0900

    define RestTemplate bean in a new config package

---
+++ src/main/java/com/bumsoap/tacoAPIclient/config/Utility.java
@@ -0,0 +1,13 @@
+package com.bumsoap.tacoAPIclient.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class Utility {
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
