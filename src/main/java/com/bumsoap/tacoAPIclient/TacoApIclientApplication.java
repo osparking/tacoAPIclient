f13605668a224bfe25b222b799d46bf5922d9555
osparking <jbpark03@gmail.com>
2024-09-20 10:36:39 +0900

    Client project for Taco Cloud API - initial

---
+++ src/main/java/com/bumsoap/tacoAPIclient/TacoApIclientApplication.java
@@ -0,0 +1,13 @@
+package com.bumsoap.tacoAPIclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoApIclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TacoApIclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bumsoap/tacoAPIclient/TacoApIclientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bumsoap.tacoAPIclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoApIclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
