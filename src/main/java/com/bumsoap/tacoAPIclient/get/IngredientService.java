a1767fa53fdb145e9e4a1b2dc0d3722a1a7245af
osparking <jbpark03@gmail.com>
2024-09-20 15:41:19 +0900

    use API for getting Ingredient by ID - Map version

--- src/main/java/com/bumsoap/tacoAPIclient/get/IngredientService.java
+++ src/main/java/com/bumsoap/tacoAPIclient/get/IngredientService.java
@@ -5,13 +5,20 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import tacos.Ingredient;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Component
 @AllArgsConstructor
 public class IngredientService {
     private RestTemplate rest;
 
     public Ingredient getIngredientById(Long ingredientId) {
+        Map<String, Long> urlVariables = new HashMap<>();
+
+        urlVariables.put("id", ingredientId);
+
         return rest.getForObject("http://localhost:8080/data-api/ingredients/{id}",
-                Ingredient.class, ingredientId);
+                Ingredient.class, urlVariables);
     }
 }
