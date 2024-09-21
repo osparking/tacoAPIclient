9a6edffdbc79f95175a7ea2f9bd889b7f711a8e4
osparking <jbpark03@gmail.com>
2024-09-21 15:46:25 +0900

    add test method for put ingredient RestTemplate service method

--- src/test/java/com/bumsoap/tacoAPIclient/get/IngredientServiceTest.java
+++ src/test/java/com/bumsoap/tacoAPIclient/get/IngredientServiceTest.java
@@ -1,11 +1,13 @@
 package com.bumsoap.tacoAPIclient.get;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import tacos.Ingredient;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,6 +19,18 @@ class IngredientServiceTest {
     @Autowired
     public IngredientServiceTest(IngredientService ingredientService) {
         this.ingredientService = ingredientService;
+    }
+
+    @Test
+    public void testIngredientPut() {
+        Long ingId = 1L;
+        Ingredient ingFromDB = ingredientService.getIngredientById(ingId);
+
+        String ingName = "밀가루 또르띠아";
+        ingFromDB.setName(ingName);
+        ingredientService.updateIngredient(ingFromDB, ingId);
+
+        assertEquals(ingredientService.getIngredientById(ingId).getName(), ingName);
     }
 
     @Test
